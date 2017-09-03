5af723feaa416d8e179411d216d404b20a5caf4f
Goran Angelkovski <goran@scaffolder.io>
2017-09-03 13:43:56 +0200

    Removed .pypirc file

--- setup.py
+++ setup.py
@@ -2,13 +2,25 @@ from setuptools import setup, find_packages
 
 setup(
     name = "scaffolder",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Goran Angelkovski",
     author_email = "goran@scaffolder.io",
-    description = "This is an interactive command line tool for rapid prototyping and automatic code generation used for creating full-stack web, mobile, TV and other apps. It can be used to create wire-frames as well by utilizing any existing front-end or back-end framework.",
+    description = "High level project and application modeling tool based on the rules of Scaffolder DSL",
     keywords = ['code generator', 'scaffolding', 'automatic coding'],
     license = "",
     url = "https://scaffoder.io",
     scripts = ['scaffolder'],
-    packages = find_packages()
+    packages = find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Code Generators'
+    ],
 )
