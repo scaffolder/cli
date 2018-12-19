a31104cfb09fcf1c900894f9c0445ee2e6a05541
Goran Angelkovski <goran@MacBook-Air.local>
2018-12-19 21:47:05 +0100

    New changes

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Docker image for Scaffolder CLI
 
 FROM python:2.7
-MAINTAINER Goran Angelkovski <goran@scaffolder.io>
+LABEL Goran Angelkovski <goran@scaffolder.io>
 
 RUN pip install scaffolder
 
--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author = "Goran Angelkovski",
     author_email = "goran@scaffolder.io",
     description = "High level project and application modeling tool based on the rules of Scaffolder DSL which utilizes Scaffolder aPaaS to generate all source code, configuration and testing files.",
+    long_description = "Interactive command line tool for rapid prototyping and automatic code generation used for creating full-stack web, mobile, TV and other apps. It can be used to create wire-frames as well by utilizing any existing front-end or back-end framework. It can generate boilerplate skeleton for your project according to strict rules and naming conventions for the files and folders been used. Trough a simple and easy command line oriented interface it creates multiple YAML files and based on it generates all the code and configuration files in a predefined directory tree.",
     keywords = ['code generator', 'scaffolding', 'automatic coding'],
     license = "MIT",
     url = "https://scaffoder.io",
